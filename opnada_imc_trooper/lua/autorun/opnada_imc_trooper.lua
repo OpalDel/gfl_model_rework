4d39740b92822fe868f685e2e92094db74b0efb1
OpalDel <105215487+OpalDel@users.noreply.github.com>
2023-10-01 09:56:30 -0500

    IMC, ST, Shared init
    IMC 6 models
    ST 6 models, white & black armor shared
    Shared pack

---
+++ opnada_imc_trooper/lua/autorun/opnada_imc_trooper.lua
@@ -0,0 +1,29 @@
+
+-- Credit to Nada - 		https://steamcommunity.com/profiles/76561198069118814
+
+
+-- Stormtrooper - 			https://steamcommunity.com/sharedfiles/filedetails/?id=2534420429
+player_manager.AddValidModel( "opnada_imc_private",						"models/opnada/IMC/private/IMC_private.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_imc_private",						"models/opnada/IMC/private/IMC_private.mdl" )
+player_manager.AddValidHands( "opnada_imc_private", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_imc_sergeant",						"models/opnada/IMC/sergeant/IMC_sergeant.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_imc_sergeant",						"models/opnada/IMC/sergeant/IMC_sergeant.mdl" )
+player_manager.AddValidHands( "opnada_imc_sergeant", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+-- Jumptrooper - 			https://steamcommunity.com/sharedfiles/filedetails/?id=2725110621
+player_manager.AddValidModel( "opnada_imc_jumptrooper",						"models/opnada/imc/jumptrooper/imc_jumptrooper.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_imc_jumptrooper",						"models/opnada/imc/jumptrooper/imc_jumptrooper.mdl" )
+player_manager.AddValidHands( "opnada_imc_jumptrooper", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_imc_lieutenant",						"models/opnada/IMC/lieutenant/IMC_lieutenant.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_imc_lieutenant",						"models/opnada/IMC/lieutenant/IMC_lieutenant.mdl" )
+player_manager.AddValidHands( "opnada_imc_lieutenant", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_imc_officer",						"models/opnada/IMC/officer/IMC_officer.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_imc_officer",						"models/opnada/IMC/officer/IMC_officer.mdl" )
+player_manager.AddValidHands( "opnada_imc_officer", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_imc_commander",						"models/opnada/IMC/commander/IMC_commander.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_imc_commander",						"models/opnada/IMC/commander/IMC_commander.mdl" )
+player_manager.AddValidHands( "opnada_imc_commander", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
---
+++ opnada_stormtrooper/lua/autorun/opnada_st_trooper.lua
@@ -0,0 +1,28 @@
+
+-- Credit to Nada - 		https://steamcommunity.com/profiles/76561198069118814
+
+-- Stormtrooper - 			https://steamcommunity.com/sharedfiles/filedetails/?id=2534420429
+player_manager.AddValidModel( "opnada_st_private",						"models/opnada/ST/private/st_private.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_st_private",						"models/opnada/ST/private/st_private.mdl" )
+player_manager.AddValidHands( "opnada_st_private", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_st_sergeant",						"models/opnada/ST/sergeant/st_sergeant.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_st_sergeant",						"models/opnada/ST/sergeant/st_sergeant.mdl" )
+player_manager.AddValidHands( "opnada_st_commander", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+-- Scouttrooper - 			https://steamcommunity.com/sharedfiles/filedetails/?id=2681187758
+player_manager.AddValidModel( "opnada_st_scout",						"models/opnada/st/scout/st_scout.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_st_scout",						"models/opnada/st/scout/st_scout.mdl" )
+player_manager.AddValidHands( "opnada_st_scout", "models/opnada/st/scout/hands/st_scout_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_st_lieutenant",						"models/opnada/ST/lieutenant/ST_lieutenant.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_st_lieutenant",						"models/opnada/ST/lieutenant/ST_lieutenant.mdl" )
+player_manager.AddValidHands( "opnada_st_lieutenant", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_st_officer",						"models/opnada/ST/officer/ST_officer.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_st_officer",						"models/opnada/ST/officer/ST_officer.mdl" )
+player_manager.AddValidHands( "opnada_st_officer", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
+
+player_manager.AddValidModel( "opnada_st_commander",						"models/opnada/ST/commander/ST_commander.mdl" ) 
+list.Set( "PlayerOptionsModel",  "opnada_st_commander",						"models/opnada/ST/commander/ST_commander.mdl" )
+player_manager.AddValidHands( "opnada_st_commander", "models/opnada/shared_hands/stormtrooper_arms.mdl", 0, "00000000" )
